43636202a93dca72c6e40227afa1df1985d67213
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2023-05-25 14:31:12 +0400

    Database setup, page.tx cleanup

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+// from Prisma documentation. Need for HMR in Next.js
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  })
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
